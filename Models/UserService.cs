48f83506f15eaf80088513165a2ec7b3fc9df6c3
Ilyés Máté <mate.ilyes@nanoworx.hu>
2026-05-20 15:39:06 +0200

    Initial Blazor users app

---
+++ Models/UserService.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public bool IsValidMail => Email.Contains("@");
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using BlazorApp1.Models;
+
+namespace BlazorApp1.Services
+{
+    public class UserService
+    {
+        private List<User> users = new() { new User() { Email = "asdas", Name = "fxdvgxvxg", Id = 42 } };
+        private int nextId = 1;
+
+        public List<User> GetAll() => users;
+
+        public void Add(string name, string email)
+        {
+            users.Add(new User
+            {
+                Id = nextId++,
+                Name = name,
+                Email = email
+            });
+        }
+
+        public void Delete(int id)
+        {
+            var user = users.FirstOrDefault(x => x.Id == id);
+            if (user != null)
+                users.Remove(user);
+        }
+    }
+}
