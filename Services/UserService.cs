365734405753565af2b4c82da8d22668608a69e6
Ilyés Máté <mate.ilyes@nanoworx.hu>
2026-05-27 13:05:36 +0200

    a

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -4,8 +4,8 @@ namespace BlazorApp1.Services
 {
     public class UserService
     {
-        private List<User> users = new() { new User() { Email = "asdas", Name = "fxdvgxvxg", Id = 42 } };
-        private int nextId = 1;
+        private List<User> users = new() { new User() { Email = "asdas", Name = "fxdvgxvxg", Id = 1 } };
+        private int nextId = 2;
 
         public List<User> GetAll() => users;
 
@@ -23,11 +23,30 @@ namespace BlazorApp1.Services
             });
         }
 
+        public void Update(int id, string name, string email)
+        {
+            var user = users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            if (!string.IsNullOrEmpty(email) && users.Any(x => x.Email == email && x.Id != id))
+            {
+                throw new Exception("No no! Van már ilyen mail!!");
+            }
+
+            user.Name = name;
+            user.Email = email;
+        }
+
         public void Delete(int id)
         {
             var user = users.FirstOrDefault(x => x.Id == id);
             if (user != null)
+            {
                 users.Remove(user);
+            }
         }
     }
 }
