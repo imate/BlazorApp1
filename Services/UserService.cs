3499a6148fc68574e348e1327937c5311fa03e5d
Ilyés Máté <mate.ilyes@nanoworx.hu>
2026-05-27 16:17:44 +0200

    xysys

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -4,14 +4,35 @@ namespace BlazorApp1.Services
 {
     public class UserService
     {
-        private List<User> users = new() { new User() { Email = "asdas", Name = "fxdvgxvxg", Id = 1 } };
-        private int nextId = 2;
+        private List<User> users;
+        private int nextId;
+
+        public UserService()
+        {
+            string name = "Teszt Elek";
+            users = new() { new User() { Name = name, Email = GenerateMail(name), Id = 1 } };
+            nextId = users.Max(x => x.Id) + 1;
+        }
 
         public List<User> GetAll() => users;
 
+        public string GenerateMail(string name)
+        {
+            string result;
+            int counter = 1;
+            do
+            {
+                result = name.Replace(" ", ".").ToLower() + (counter == 1 ? "" : counter) + "@nanoworx.hu";
+                counter++;
+            }
+            while (users != null && users.Any(x => x.Email == result));
+            return result;
+        }
+
         public void Add(string name, string email)
         {
-            if (!string.IsNullOrEmpty(email) && users.Any(x => x.Email == email)) {
+            if (!string.IsNullOrEmpty(email) && users.Any(x => x.Email == email))
+            {
                 throw new Exception("No no! Van már ilyen mail!!");
             }
 
